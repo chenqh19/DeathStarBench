ef117532a531c1739ab92fa0899e5aadaab43722
chenqh23 <chenqh23@localhost>
2023-10-13 19:25:15 -0600

    update autosum

--- hotelReservation/profile/calculate.sh
+++ hotelReservation/profile/calculate.sh
@@ -1,34 +1,25 @@
 #!/bin/bash
 
-if [ "$#" -ne 3 ]; then
-  echo "Usage: $0 <filename> <service_string> <func_string>"
+if [ "$#" -lt 3 ]; then
+  echo "Usage: $0 <filename> <service_string> <func_string1> [func_string2] [func_string3] ..."
   exit 1
 fi
 
 filename="$1"
 service_string="$2"
-func_string="$3"
+shift 2  # Remove the first two arguments (filename and service_string)
 
-# Filter lines containing $service_string from the specified file
-grep "$service_string" "$filename" > filtered.txt
+# Combine all func_string arguments into a single regular expression
+func_strings_regex=$(printf "|%s" "$@")
+func_strings_regex=${func_strings_regex:1}  # Remove the leading '|'
 
-# Filter lines containing $func_string from the filtered file
-grep "$func_string" filtered.txt > filtered_func.txt
-
-# # Calculate the sum of numbers before the percent sign at the beginning of each line
-# total=0
-# while read -r line; do
-#     # Extract the number before the percent sign at the beginning of each line
-#     number=$(echo "$line" | grep -o '.*')
-    
-#     # Add the number to the total
-#     total=$((total + number))
-# done < filtered_func.txt
+# Filter lines containing $service_string and any of the func_strings
+grep -E "$service_string.*($func_strings_regex)" "$filename" > filtered_func.txt
 
 total=$(awk -F'%' '{ total += $1 } END { print total }' filtered_func.txt)
 
 # Print the total
 echo "Total: $total"
 
-Clean up temporary files
-rm filtered.txt filtered_func.txt
+# Clean up temporary files
+rm filtered_func.txt
