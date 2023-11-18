151a33465cf0605670a82c88e187eb4ba3e1f663
chenqh23 <chenqh23@localhost>
2023-11-18 12:34:48 -0700

    trace compose post

--- scripts/draw.py
+++ scripts/draw.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_multiple_box_plots(data_sets, output_file, y_threshold):
-    fig, ax = plt.subplots(figsize=(5, 6))
+    fig, ax = plt.subplots(figsize=(12, 6))
     over_vals = []
     for i, dataset in enumerate(data_sets):
         if dataset[-1] > y_threshold:
@@ -15,11 +15,14 @@ def plot_multiple_box_plots(data_sets, output_file, y_threshold):
     bplot = ax.boxplot(data_sets, vert=True, widths=0.5, patch_artist=True)
 
     # ax.set_xticklabels([f'Data {i+1}' for i in range(len(data_sets))])
-    ax.set_xticklabels(["1-node\n10k posts", "1-node\n200k posts", "2-node\n10k posts", "2-node\n200k posts"])
-    ax.set_ylabel('Remote function call overhead')
+    # ax.set_xticklabels(["1-node\n10k posts", "1-node\n200k posts", "2-node\n10k posts", "2-node\n200k posts"])
+    ax.set_xticklabels(["text", "user-mention", "creator", "media", "urls", "unique-id", 
+                        "store-post", "write-utl", "social-graph", "write-htl"])
+    ax.set_ylabel('Remote function call overhead (us)')
 
     # Customize box colors
-    colors = ['lightblue', 'lightblue', 'lightgreen', 'lightgreen']
+    colors = ["lightcyan", 'lightsteelblue', 'lightskyblue', 'lightseagreen', 'lightgreen', 
+                'lightyellow', 'lightpink', 'lightsalmon', 'lightgray', 'lightslategray']
     for patch, color in zip(bplot['boxes'], colors):
         patch.set_facecolor(color)
 
@@ -34,17 +37,30 @@ def plot_multiple_box_plots(data_sets, output_file, y_threshold):
                         arrowprops=dict(facecolor='black', arrowstyle='->'))
         i += 1
 
-    plt.title('HomeTimeline calls Read_Post')
+    plt.title('Function call at 2-node, 200k posts')
     plt.savefig(output_file)
     plt.show()
 
 # Example usage with multiple data sets
-data_set1 = [96, 220, 247, 277, 1360]
-data_set2 = [169, 225, 246, 268, 4138]
-data_set3 = [225, 328, 366, 412, 5872]
-data_set4 = [231, 329, 372, 425, 8193]
+# data_set1 = [96, 220, 247, 277, 1360]
+# data_set2 = [169, 225, 246, 268, 4138]
+# data_set3 = [225, 328, 366, 412, 5872]
+# data_set4 = [231, 329, 372, 425, 8193]
 
-output_file = 'fig_vertical_with_threshold.jpg'
-y_threshold = 500  # Adjust this threshold as needed
+datasets = [
+    [183, 286, 351, 402, 555],
+    [161, 249, 294, 341, 784],
+    [170, 263, 313, 361, 790],
+    [166, 247, 296, 343, 718],
+    [167, 292, 344, 394, 637],
+    [155, 223, 278, 318, 1401],
+    [152, 251, 291, 327, 1783],
+    [144, 225, 267, 321, 1378],
+    [161, 252, 284, 334, 716],
+    [140, 241, 293, 341, 1443]
+]
 
-plot_multiple_box_plots([data_set1, data_set2, data_set3, data_set4], output_file, y_threshold)
+output_file = 'fig_2-10.jpg'
+y_threshold = 800  # Adjust this threshold as needed
+
+plot_multiple_box_plots(datasets, output_file, y_threshold)
