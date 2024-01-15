a7a4e1df99920a13b67d5ea81e3ed93585e695d5
chenqh23 <chenqh23@localhost>
2024-01-14 18:06:48 -0700

    update

--- scripts/setCAT.py
+++ scripts/setCAT.py
@@ -5,8 +5,8 @@ only_cdp = False
 
 max_part = 16
 core_llc = [20, 20] # number of core & llc ways
-core_part = [10, 3, 3] 
-llc_part = [12, 4, 4]
+core_part = [6, 1, 13] 
+llc_part = [5, 1, 14]
 if cdp:
     cdp_d_part = [1, 2, 3]
     assert len(cdp_d_part) == len(llc_part)
@@ -33,6 +33,7 @@ for process in processes:
     unset_cmd = "sudo taskset -cp 0-19 " + process
     subprocess.run(unset_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 
+'''
 # if we do not use all cores, the rest of the cores should be put into the last partition
 core_rest = core_llc[0]
 for core in core_part:
@@ -85,14 +86,14 @@ if cdp:
         print(i)
         cdp_d_hex.append(mixed_hex[2*i])
         cdp_c_hex.append(mixed_hex[2*i+1])
-core_str = ""
+core_str = "\""
 llc_str = ""
 cdp_str = ""
 set_core_cmds = []
 
 cnt_core = 0
 for i in range(len(llc_hex)):
-    core_str = core_str + "\"llc:" + str(i) + "=" + str(cnt_core) + "-" + str(cnt_core+core_part[i]-1) + ";\""
+    core_str = core_str + "llc:" + str(i) + "=" + str(cnt_core) + "-" + str(cnt_core+core_part[i]-1) + ";"
     llc_str = llc_str + "\"llc:" + str(i) + "=" + str(llc_hex[i]) + ";\""
     if cdp:
         cdp_str = cdp_str + "\"llc:" + str(i) + "d=" + str(cdp_d_hex[i]) + ";"+ "llc:" + str(i) + "c=" + str(cdp_c_hex[i]) + ";\""
@@ -103,29 +104,26 @@ for i in range(len(llc_hex), max_part):
     llc_str = llc_str + "\"llc:" + str(i) + "=" + str(hex(2**core_llc[1]-1)) + ";\""
     if cdp:
         cdp_str = cdp_str + "\"llc:" + str(i) + "d=" + str(hex(2**core_llc[1]-1)) + ";"+ "llc:" + str(i) + "c=" + str(hex(2**core_llc[1]-1)) + ";\""
+core_str = core_str + "\""
 
-
+print(core_str)
+print(llc_str)
 core_cmd = "sudo pqos -a " + core_str
 llc_cmd = "sudo pqos -e " + llc_str
 if cdp:
     cdp_cmd = "sudo pqos -e " + cdp_str
 # CDP example: pqos -e "llc:1d=0xfff;llc:1c=0xfff00;" (add d(data) or c(code) after LLC index)
 # ref: https://github.com/intel/intel-cmt-cat/wiki/Usage-Examples
-print(core_cmd)
-print(llc_cmd)
-print(cdp_cmd)
-print(set_core_cmds)
 
-
-core = subprocess.run(core_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-print(core.stderr)
 if cdp:
     subprocess.run("sudo pqos -R l3cdp-on", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     partition_cmd = cdp_cmd
 else:
     subprocess.run("sudo pqos -R l3cdp-off", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     partition_cmd = llc_cmd
+subprocess.run(core_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 subprocess.run(partition_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 for cmd in set_core_cmds:
     subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
 subprocess.run("sudo pqos -s", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+'''
