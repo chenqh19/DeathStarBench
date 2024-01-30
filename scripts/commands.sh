2b4d5c4a90e66cab5a59cf807aeb44d2ef6620fd
chenqh23 <chenqh23@localhost>
2024-01-30 09:17:32 -0700

    update

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -9,5 +9,6 @@ sudo docker run --net=host weaveworksdemos/load-test -h localhost -r 100 -c 2
 # hotel reservation
 ../wrk2/wrk -D exp -t 100 -c 100 -d 30 -L -s ./wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://127.0.0.1:5000 -R 3000
 # kubernetes
+sudo kubeadm token create --print-join-command # on the host
 kubectl patch svc frontend -p '{"spec":{"externalIPs":["192.168.0.194"]}}'
 ../wrk2/wrk -D exp -t 100 -c 100 -d 30 -L -s ./wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://192.168.0.194:5000 -R 3000
