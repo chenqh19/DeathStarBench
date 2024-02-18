cc70d9ed50b79d19069ad5ed8a86f454ef20167a
chenqh23 <chenqh23@localhost>
2024-02-17 18:30:51 -0700

    socialnet rpc

--- scripts/batched_test.py
+++ scripts/batched_test.py
@@ -9,11 +9,20 @@ def set_range(low, high, interval):
     rpss.append(high)
     return rpss
 
-
-output_file = "../kuber-deploy/test_rps.txt"
-rpss = set_range(1900, 2200, 100)
+test_type = "ps" 
+output_file = "../lat_files/"+test_type+"_rps.txt"
+rpss = set_range(1000, 2200, 100)
 for rps in rpss:
-    cmd = "../../wrk2/wrk -D exp -t 100 -c 100 -d 20 -L -s ../../hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://192.168.0.194:5000 -R " + str(rps)
-    call_py_cmd = "cd llc ; python gen_work.py \"" + cmd + "\" " + output_file + " 0"
+    if test_type == "hr":
+        cmd = "../../wrk2/wrk -D exp -t 100 -c 100 -d 20 -L -s ../../hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua http://192.168.0.194:5000 -R " + str(rps)
+    elif test_type == "htl" or test_type == "ps":
+        cmd = "../../wrk2/wrk -D exp -t 100 -c 100 -d 20 -L -s ../../socialNetwork/wrk2/scripts/social-network/read-home-timeline.lua http://localhost:8080/wrk2-api/home-timeline/read -R " + str(rps)
+    elif test_type == "utl":
+        cmd = "../../wrk2/wrk -D exp -t 100 -c 100 -d 20 -L -s ../../socialNetwork/wrk2/scripts/social-network/read-user-timeline.lua http://localhost:8080/wrk2-api/user-timeline/read -R " + str(rps)
+    call_py_cmd = "cd llc ; python gen_work.py \"" + cmd + "\" " + output_file 
+    if test_type == "hr":
+        call_py_cmd = call_py_cmd + " 0"
+    else:
+        call_py_cmd = call_py_cmd + " 1"
     print(call_py_cmd)
     subprocess.run(call_py_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
--- scripts/llc/extract.py
+++ scripts/llc/extract.py
@@ -8,38 +8,45 @@ def find_second_smallest(numbers):
     else:
         return None
 
+def find_mid_avg(numbers):
+    unique_numbers = sorted(set(numbers))
+    if len(unique_numbers) > 1:
+        return round((unique_numbers[1]+unique_numbers[2]+unique_numbers[3])/3, 1)
+    else:
+        return None
+
 def main():
     count = 0
     results = []
-    with open('./llc_sweep-utl5-1.txt', 'r') as file:
+    with open('../lat_files/ps_rps-local.txt', 'r') as file:
         lines = file.readlines()
         for i in range(len(lines)):
             if "new_config" in lines[i]:
                 if i + 5 < len(lines):
                     numbers = [float(line.split()[0]) for line in lines[i+1:i+6]]
-                    second_smallest = find_second_smallest(numbers)
-                    if second_smallest is not None:
-                        results.append(second_smallest)
+                    rep_num = find_mid_avg(numbers)
+                    if rep_num is not None:
+                        results.append(rep_num)
                     else:
                         print(f"Not enough numbers at {count} to find the latency.")
                 count += 1
     print(results)
-    arr_size = 6
-    final_array = np.zeros((arr_size, arr_size))
-    c = 0
-    # no_part_result = results[0]
-    # c = 1
-    row_len = arr_size 
-    while row_len > 0:
-        print(row_len, c)
-        for i in range(c, c+row_len):
-            final_array[i-c][row_len-1-(i-c)] = results[i]
-        c = c+row_len
-        row_len -= 1
-    print(final_array)
-    plt.imshow(final_array, cmap='viridis', interpolation='nearest', vmin=15, vmax=50)
-    plt.colorbar()
-    plt.savefig('heatmap.png')
+    # arr_size = 6
+    # final_array = np.zeros((arr_size, arr_size))
+    # c = 0
+    # # no_part_result = results[0]
+    # # c = 1
+    # row_len = arr_size 
+    # while row_len > 0:
+    #     print(row_len, c)
+    #     for i in range(c, c+row_len):
+    #         final_array[i-c][row_len-1-(i-c)] = results[i]
+    #     c = c+row_len
+    #     row_len -= 1
+    # print(final_array)
+    # plt.imshow(final_array, cmap='viridis', interpolation='nearest', vmin=15, vmax=50)
+    # plt.colorbar()
+    # plt.savefig('heatmap.png')
 
 
 
